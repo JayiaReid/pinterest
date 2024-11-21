3cd5dfde8d27282c3fd8ce47e8e25edf55802a88
JayiaReid <jayiareid24@gmail.com>
2024-11-21 00:01:54 -0500

    adjusting board cover logic

--- app/api/pin/save/route.js
+++ app/api/pin/save/route.js
@@ -3,21 +3,21 @@ import Pin from "@/models/Pin";
 import Section from "@/models/Section";
 import user_board from "@/models/user_board";
 
-export async function PUT(req){
+export async function PUT(req) {
 
     await PinterestDB()
 
     try {
         const body = await req.json()
 
-        const {pin, _id} = body
+        const { pin, _id } = body
         console.log(body)
 
         const thisPin = await Pin.findById(pin)
 
         let destination = await user_board.findById(_id)
-        
-        if(destination){
+
+        if (destination) {
             destination.pins.push(pin)
 
             if (destination.pins.length <= 3) {
@@ -25,7 +25,7 @@ export async function PUT(req){
                 const secondPin = destination.pins[1] ? await Pin.findById(destination.pins[1]) : null;
                 const third = destination.pins[2] ? await Pin.findById(destination.pins[2]) : null
 
-                if(destination.cover == '/blank.jpg'){
+                if (destination.cover == '/blank.jpg') {
                     if (firstPin) {
                         destination.cover = firstPin.image
                     }
@@ -35,18 +35,27 @@ export async function PUT(req){
                     if (third) {
                         destination.images[1] = third.image
                     }
-                }else{
+                } else {
+
                     if (firstPin) {
-                        destination.images[0] = firstPin.image
-                    }
-    
-                    if (secondPin) {
-                        destination.images[1] = secondPin.image
+                        if (destination.cover != firstPin.image) {
+                            destination.images[0] = firstPin.image
+                            if (secondPin) {
+                                destination.images[1] = secondPin.image
+                            }
+                        } else {
+                            if (secondPin) {
+                                destination.images[0] = secondPin.image
+                            }
+                            if (third) {
+                                destination.images[1] = third.image
+                            }
+                        }
                     }
                 }
-               
+
             }
-        }else if(!destination){
+        } else if (!destination) {
             destination = await Section.findById(_id)
             destination.pins.push(pin)
 
