b4f33a9dc4b74b9c8473d400274c42620bb46507
JayiaReid <jayiareid24@gmail.com>
2024-11-27 00:28:10 -0500

    .

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,7 @@ import { useSignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen bg-[url('/landing.jpg')] bg-cover bg-center">
+    <div className="flex items-center justify-center h-screen bg-[url('/landing.png')] bg-cover bg-center">
       <div>
         <SignIn afterSignInUrl={`/`} signUpUrl='/sign-up'/>
       </div>
--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen bg-[url('/landing.jpg')] bg-cover bg-center">
+    <div className="flex items-center justify-center h-screen bg-[url('/landing.png')] bg-cover bg-center">
       <div className="">
         <SignUp afterSignUpUrl="/" signInUrl="/sign-in" />
       </div>
